afe89bd17e22523f54498114700f2b8edde3a65b
Chaitanya Karthik <chaitanyakarthikbasva@gmail.com>
2026-06-15 16:38:38 +0530

    second commit and I am learning it

--- src/main/java/com/javalearning/Beans/Vehicle.java
+++ src/main/java/com/javalearning/Beans/Vehicle.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Lazy
 public class Vehicle {
-
+//  I am adding this comment to check the changes in git and I ll push it nigger
     public Vehicle(){
         System.out.println("...VEHICLE BEAN CREATED...");
     }
